d501fb66be6da02034d65f5d22af696bd3d050ef
Namo Narayan <namonarayan@Namos-MacBook-Pro.local>
2024-01-04 08:50:09 +0530

    backend update

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const config = {
     // backend_url : "http://localhost:4000/api/v1"
-    backend_url : "https://todobackend-ncfx.onrender.com"
+    backend_url : "https://todobackend-ncfx.onrender.com/api/v1"
 
 }
